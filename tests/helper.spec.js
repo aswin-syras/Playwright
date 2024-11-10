f33efa37619791d91c8fc688f30a1095a0ff41d8
Aswin Syras <aswinsyras@gmail.com>
2024-11-10 16:04:09 -0500

    passed parameter

--- tests/helper.spec.js
+++ tests/helper.spec.js
@@ -1,13 +1,13 @@
 const {test,expect} = require ('@playwright/test');
 module.exports = { login };
 
-async function login(page){
+async function login(page, username, password){
     await page.goto('https://staging-demo.yuja.com/'); 
     //test.use({ navigationTimeout: 10000 });
     await expect(page).toHaveTitle('Xavier University Enterprise Video Platform')
     await page.locator('id=action-btn').nth(1).click();
-    await page.locator('id=loginuserid').fill("aswin_syras");
-    await page.locator('id=password').fill("Aswinsyras@1231");
+    await page.locator('id=loginuserid').fill(username);
+    await page.locator('id=password').fill(password);
     await page.locator('id=loginButton').click();
     const avatar = await page.locator('id=avatarImage');
     await expect(avatar).toBeVisible({timeout: 50000});
--- tests/testscript.spec.js
+++ tests/testscript.spec.js
@@ -2,6 +2,6 @@ const {test,expect} = require ('@playwright/test');
 import {login} from "./helper.spec";
 
 test('First Page',async ({page}) => {
-    await login(page);
+    await login(page, "aswin_syras", "Aswinsyras@1231");
     //comments added
 })
